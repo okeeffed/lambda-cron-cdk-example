73a33c60720aa196b3e24d40bf712f8cb4cdc4cd
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-06-20 13:53:08 +1000

    feat: Attempt running every 5 minutes

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import events = require('@aws-cdk/aws-events');
 import targets = require('@aws-cdk/aws-events-targets');
 import lambda = require('@aws-cdk/aws-lambda');
 import cdk = require('@aws-cdk/core');
-import fs = require('fs');
 
 require('dotenv').config();
 
@@ -11,12 +10,10 @@ export class LambdaCronStack extends cdk.Stack {
     super(app, id);
 
     const lambdaFn = new lambda.Function(this, 'Singleton', {
-      code: new lambda.InlineCode(
-        fs.readFileSync('lambda-handler.js', { encoding: 'utf-8' }),
-      ),
-      handler: 'index.handler',
+      code: lambda.Code.asset('resources'),
+      handler: 'slack-message.handler',
       timeout: cdk.Duration.seconds(300),
-      runtime: lambda.Runtime.NODEJS_10_X,
+      runtime: lambda.Runtime.NODEJS_12_X,
       environment: {
         SLACK_CHANNEL: process.env.SLACK_CHANNEL as string,
       },
@@ -25,7 +22,7 @@ export class LambdaCronStack extends cdk.Stack {
     // Run every day at 6PM UTC
     // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
     const rule = new events.Rule(this, 'Rule', {
-      schedule: events.Schedule.expression('cron(0/10 * ? * ? *)'),
+      schedule: events.Schedule.expression('cron(0/5 * ? * * *)'),
     });
 
     rule.addTarget(new targets.LambdaFunction(lambdaFn));
